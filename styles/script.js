2f925a07f4f061a70b0b64036ed69cc6e63bca76
Sayam Gautam <sayamgautam1@gmail.com>
2022-04-19 21:34:53 +0930

    add function to display the api searches and create HTML elements to display the search in the document

--- styles/script.js
+++ styles/script.js
@@ -1,7 +1,7 @@
-
 var apiKey = "2200016b";
-
-
+let mediaImageEl = document.querySelector("#movie-image");
+let mediaTitleEl = document.querySelector("#movie-title");
+let movieSearchedEl = document.querySelector("#movie-search");
 // function to get the movie name input from the search bar
 function handleFormSubmit(form) {
   const movieName = form.querySelector("#input-search").value;
@@ -11,15 +11,30 @@ function handleFormSubmit(form) {
 function getApi(movieName) {
   var requestUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${movieName}`;
 
-  fetch(requestUrl)
-  .then(function (response) {
+  fetch(requestUrl).then(function (response) {
     response.json().then(function (data) {
       console.log(data);
+      displayMovies(data);
+    });
+  });
+}
 
-      
-    
-    })
-    
-  })
+function displayMovies(data) {
+  movieSearchedEl.innerText = "";
+  for (i = 0; i < data.Search.length; i++) {
+    let mediaDetails = {
+      mediaTitle: data.Search[i].Title,
+      mediaImage: data.Search[i].Poster,
+    };
+    let movieSearchContainer = document.createElement("div");
+    movieSearchContainer.classList.add("search-display");
+    // get movie title
+    let movieTile = document.createElement("h3");
+    movieTile.appendChild(document.createTextNode(mediaDetails.mediaTitle));
+    movieSearchContainer.appendChild(movieTile);
 
+    //get image backgroung
+    movieSearchContainer.style.backgroundImage = `url(${mediaDetails.mediaImage})`;
+    movieSearchedEl.appendChild(movieSearchContainer);
+  }
 }
