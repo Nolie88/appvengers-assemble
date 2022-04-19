0e8627d19ab87451fd5aeb6b208e0c925cfa9712
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-04-19 20:25:02 +1000

    first fetch

--- styles/script.js
+++ styles/script.js
@@ -1,5 +1,25 @@
+
+var apiKey = "2200016b";
+
+
 // function to get the movie name input from the search bar
 function handleFormSubmit(form) {
   const movieName = form.querySelector("#input-search").value;
-  console.log(movieName);
+  getApi(movieName);
+}
+
+function getApi(movieName) {
+  var requestUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${movieName}`;
+
+  fetch(requestUrl)
+  .then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+
+      
+    
+    })
+    
+  })
+
 }
